494d8b963e8ce8c7e4f9bdb11b2a5f5ab5e5a718
mannyavila <mannyavila90@gmail.com>
2023-06-12 12:41:05 -0400

    Mostrar los gastos

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ function App() {
 
   const guardarGastos = gasto => {
     gasto.id = generarId();
+    gasto.fecha = Date.now();
     setGastos([...gastos, gasto])
 
     setAnimarModal(false)
--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Gasto = () => {
+const Gasto = ({gasto}) => {
+    const { categoria, nombre, cantidad, id } = gasto;
   return (
-    <div>
-      <p>gastosjsx</p>
+    <div className="gasto sombra">
+      <div className="contenido-gasto">
+        <div className="descripcion-gasto">
+            <p className="categoria">{categoria}</p>
+            <p className="nombre-gasto">{nombre}</p>
+        </div>
+      </div>
     </div>
   )
 }
