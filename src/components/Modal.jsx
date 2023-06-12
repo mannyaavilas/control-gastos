3ef7db71198ec066069cecc30ff1e1bcdbfd7d6c
mannyavila <mannyavila90@gmail.com>
2023-06-11 20:42:10 -0400

    State para los gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react'
 import BtnCerrar from '../img/cerrar.svg'
 
 const Modal = ({setModal, animarModal, setAnimarModal}) => {
+
+  const [ nombre, setNombre ] = useState('');
+  const [ cantidad, setCantidad ] = useState('');
+  const [ categoria, setCategoria ] = useState('');
 
   const ocultarModal = () => {
     
@@ -31,6 +36,8 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
             id='nombre'
             type="text"
             placeholder='Añade el nombre del gasto'
+            value={nombre}
+            onChange={ e => setNombre(e.target.value)}
           />
         </div>
 
@@ -41,12 +48,18 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
             id='cantidad'
             type="number"
             placeholder='Añade la cantidad del gasto: ej. 100'
+            value={cantidad}
+            onChange={ e => setCantidad(Number(e.target.value))}
           />
         </div>
         <div className="campo">
           <label htmlFor="categoria">Categoria</label>
 
-          <select id="categoria">
+          <select 
+            id="categoria"
+            value={categoria}
+            onChange={ e => setCategoria(e.target.value)}
+            >
             <option value="">-- Seleccione --</option>
             <option value="ahorro">Ahorro</option>
             <option value="comida">Comida</option>
