b2b45555d0392900c4e3fe32158b43545e4120b6
mannyavila <mannyavila90@gmail.com>
2023-06-13 17:48:25 -0400

    Personalizar grafica y texto añadido

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { CircularProgressbar } from 'react-circular-progressbar';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 const ControlPresupuesto = ({gastos, presupuesto}) => {
@@ -35,8 +35,15 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
         <div className="">
-        <CircularProgressbar 
-            value={porcentaje}/>;
+            <CircularProgressbar 
+                styles={buildStyles({
+                    pathColor: '#3B82F6',
+                    trailColor: '#F5F5F5',
+                    textColor: '#3B82F6'
+                })}
+                value={porcentaje}
+                text={`${porcentaje}% Gastado`}
+            />;
         </div>
 
         <div className="contenido-presupuesto">
