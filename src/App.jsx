148aea5382b2574c6288ff06bc333974eb9d12d1
mannyavila <mannyavila90@gmail.com>
2023-06-13 21:10:50 -0400

    Componente Filtro para categoria de gastos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Header from './components/Header'
+import Filtros from './components/Filtros';
 import ListadoGastos from './components/ListadoGastos';
 import Modal from './components/Modal';
 import { generarId } from './helpers'
@@ -21,6 +22,8 @@ function App() {
 
   const [ gastoEditar, setGastoEditar ] = useState({});
 
+  const [ filtro, setFiltro ] = useState('');
+
   useEffect(() => {
     if ( Object.keys(gastoEditar).length > 0 ) {
       setModal(true);
@@ -38,6 +41,12 @@ function App() {
   useEffect(() => {
     localStorage.setItem('gastos', JSON.stringify(gastos) ?? [])
   }, [gastos])
+
+  useEffect(() => {
+    if (filtro) {
+      
+    }
+  },[filtro])
 
   useEffect(() => {
     const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0
@@ -94,6 +103,10 @@ function App() {
        {isValidPresupuesto && (
         <>
           <main>
+            <Filtros
+                filtro={filtro}
+                setFiltro={setFiltro}
+             />
             <ListadoGastos
                 gastos={gastos}
                 setGastoEditar={setGastoEditar}
